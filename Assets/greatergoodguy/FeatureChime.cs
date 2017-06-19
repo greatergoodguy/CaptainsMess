9e18ac1e1b037dd7ecdeb0190674e6d8511c6420
greatergoodguy <xuetomyu@gmail.com>
2017-06-18 19:21:18 -0500

    Added Chime feature to player that is activated when pressing space bar

---
+++ Assets/greatergoodguy/FeatureChime.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FeatureChime : NetworkBehaviour {
+
+	AudioSource audioSource;
+
+	void Awake() {
+		audioSource = transform.GetComponent<AudioSource>();
+	}
+
+	void Update() {
+		if (isLocalPlayer && Input.GetKeyDown(KeyCode.Space)) {
+			Chime();
+		}
+	}
+
+	public void Chime() {
+		Debug.Log("Chime()");
+		CmdChime();
+	}
+
+	[Command]
+	public void CmdChime() {
+		Debug.Log("CmdChime()");
+		RpcChime();
+	}
+
+	[ClientRpc]
+	public void RpcChime() {
+		Debug.Log("RpcFeint()");
+
+		audioSource.Play();
+	}
+}
